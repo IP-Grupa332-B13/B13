010956b264fb92fa9dca3aee05a06319fb9c321c
Nomics <tabarceacosmin@gmail.com>
2014-04-13 13:45:03 +0300

    cea
    c

--- TeamProject Soft/TeamProject Soft/AddClient.aspx.designer.cs
+++ TeamProject Soft/TeamProject Soft/AddClient.aspx.designer.cs
@@ -13,21 +13,21 @@ namespace TeamProject_Soft {
     public partial class AddClient {
         
         /// <summary>
-        /// GridView1 control.
+        /// ListView1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView GridView1;
+        protected global::System.Web.UI.WebControls.ListView ListView1;
         
         /// <summary>
-        /// SqlDataSource1 control.
+        /// LinqDataSource1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
+        protected global::System.Web.UI.WebControls.LinqDataSource LinqDataSource1;
     }
 }
